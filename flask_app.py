18cc566de695213ec2b0c61059b5dba3720324dc
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-07-16 16:56:52 +0200

    Add a basic form for user input.

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 app = Flask(__name__)
 import csv
 
@@ -16,9 +16,11 @@ dataset=read_data('Book1.csv')
 @app.route('/')
 def home():
 
+    input = request.args.get('input-variable', None)
+
     tmp = dataset[1]['food_wgt']
     
-    return render_template('home.html', output_variable=tmp)
+    return render_template('home.html', input_variable=input, output_variable=tmp)
 
 if __name__ == '__main__':
     app.debug = True

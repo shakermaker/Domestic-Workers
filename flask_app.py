f315a71e6e774bef06e2e947941d61d531910a9f
Osman G <ozmungs@gmail.com>
2014-07-21 13:52:20 +0200

    Add bootstrap
    Update Styling

--- flask_app.py
+++ flask_app.py
@@ -16,12 +16,44 @@ dataset=read_data('Book1.csv')
 @app.route('/')
 def home():
 
-    input = request.args.get('input-variable', None)
+    input = request.args.get('input-variable', 'Daily')
+    input2 = int(request.args.get('input2-variable', '0'))
+    input3 = int(request.args.get('input3-variable', '1'))
 
-    tmp = dataset[1]['food_wgt']
-    
-    return render_template('home.html', input_variable=input, output_variable=tmp)
+    minwage=1880
+    payrate=0
+
+#Assumption using DoL info - a month includes 4.33 weeks and a week is for 6 work days.
+
+    if input=="Daily":
+        payrate=input2*6*4.33
+    elif input=="Weekly":
+        payrate=input2*4.33
+    elif input=="Monthly":
+        payrate=input2
+
+    minwage_ratio=payrate/minwage
+
+#Per person costs for each expenditure type
+rent_cost=1000
+food_cost=900
+trans_cost=780
+educ_cost=100
+comm_cost=100
+health_cost=50
+
+#All costs and
+
+#Proportional costs
+food_wgt=0.45
+
+#Fixed costs
+trans_daily=30
+
+    #tmp = float(dataset[input2][input])
+
+    return render_template('bootstrap.html', input_variable=input, input2_variable=input2, input3_variable=input3, output_variable=payrate, output2_variable=food_cost)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+   app.debug = True
+   app.run()

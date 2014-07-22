a32ca7786fbd3621b261d58af1d3caf08eb2f275
Osman G <ozmungs@gmail.com>
2014-07-22 10:35:50 +0200

    Some text and variable updates
    Output statement added. And tagline changed.

--- flask_app.py
+++ flask_app.py
@@ -21,8 +21,7 @@ def home():
     input3 = int(request.args.get('input3-variable', '1'))
 
     minwage=1880
-    payrate=0
-
+    
     #Assumption using DoL info - a month includes 4.33 weeks and a week is for 6 work days.
 
     if input=="day":
@@ -38,27 +37,32 @@ def home():
     #All costs are adjustable.
 
     #Proportional costs to start with
-    food_wgt=0.45
-
     rent_cost=1000
     food_cost=900
     trans_daily=30
     educ_cost=100
     comm_cost=100
     health_cost=50
-    other_cost=200
+    other_cost=400
     
     trans_cost=trans_daily*26
     
-    totalcost=rent_cost+ input3*food_cost+trans_cost+educ_cost*2 + comm_cost*input3 + health_cost*input3 + other_cost*2
+    totalcost=rent_cost+ input3*food_cost+trans_cost+educ_cost*(input3-2) + comm_cost*input3 + health_cost*input3 + other_cost
 
     final_output=payrate/totalcost*100
 
+    if final_output>0 & final_output<=0.75:
+        statement=="You're paying too little given the living costs and the size of your domestic worker's household. You should definitely take time to reassess how much you're paying. Try our tool to figure out a fairer wage!"
+    elif final_output>0.75 & final_output<=0.9:
+        statement=="You're nearly there! Take time to reassess the wage by using our tool!"
+    elif final_output>0.9 & final_output<=1.0:
+        statement=="You're likely paying a fair wage given the living costs and your domestic worker's household size!"
+    else:
+        statement=="You're an excellent employer! You have shown that you about savings and not just sufficiency! Share your results!"
 
+   #tmp = float(dataset[input2][input])
 
-    #tmp = float(dataset[input2][input])
-
-    return render_template('bootstrap.html', input_variable=input, input2_variable=input2, input3_variable=input3, output_variable=final_output)
+    return render_template('bootstrap.html', input_variable=input, input2_variable=input2, input3_variable=input3, output_variable=final_output, output_statement=statement)
 
 if __name__ == '__main__':
     app.debug = True
